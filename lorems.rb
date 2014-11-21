db455c481150db312e67c8c6ff6a4d8a667392db
Warren MacEvoy <wmacevoy@gmail.com>
2014-11-20 19:18:24 -0700

    higher precision sum

--- lorems.rb
+++ lorems.rb
@@ -54,19 +54,30 @@ if pmax == 1.0 then
   pmax = 2.0
 end
 
-sum=0.0
+sum0=0.0
+sum1=0.0
+info=0.0
+bits=0.0
 
 while pmin <= pmax/2.0 do
+  sum0=0.0
   fc.lorems(pmax/2.0,pmax) do |word,prob|
-    sum = sum + prob
-    csv << [word,prob,sum]
+    bits =-prob*Math.log2(prob)
+    info = info + bits
+    sum0 = sum0 + prob
+    csv << [word,prob,sum0+sum1,bits,info]
   end
+  sum1=sum1+sum0
   pmax = pmax/2.0
 end
 if pmin < pmax then
+  sum0=0.0
   fc.lorems(pmin,pmax) do |word,prob|
-    sum = sum + prob
-    csv << [word,prob,sum]
+    bits =-prob*Math.log2(prob)
+    info = info + bits
+    sum0 = sum0 + prob
+    csv << [word,prob,sum0+sum1,bits,info]
   end
+  sum1=sum1+sum0
 end
 csv.close
