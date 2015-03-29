56791f487fa293e43720b6cf6fc2d6a1bbf9d9a9
quail <timidquail@gmail.com>
2015-03-29 14:38:04 -0600

    text2pa more or less finished.

--- pa/text2pa.rb
+++ pa/text2pa.rb
@@ -5,47 +5,6 @@ require_relative 'smallautomata'
 $debug = true
 $depth = 2
 
-#
-# fc - frequency of characters f[char]->count in ab.txt
-# 
-fc = Hash.new()
-
-count=0
-File.open("ab.txt").each do |line|
-  line.strip.each_char { |c| fc[c] = (fc[c]||0) + 1 }
-  count=count+1
-end
-
-if count > 0 then
-  fc[:start]=count
-  fc[:end]=count
-end
-
-#
-# wc - list of [c,w], in decreasing order
-#
-fc_norm = 0
-fc.values.each { |count| fc_norm = fc_norm + count }
-fc_norm = 1.0/fc_norm
-$wc = fc.map{ |p| [p[0],p[1]*fc_norm] }.sort { |a,b| b[1] <=> a[1] }
-
-if $debug then
-  puts "character weights: "
-  puts $wc
-end
-
-#
-# invert the wc map of index to characters (and weights)
-#
-#    wc[index]->[character,weight] iff  ic[fcharacter]->index
-#
-$ic = Hash.new
-$wc.each_with_index { |pair,index| $ic[pair[0]]=index }
-
-if $debug then
-  print "index map: "
-  puts $ic
-end
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 $wd = Hash.new()
 $at = Array.new($depth,:start)
@@ -74,8 +33,36 @@ if $debug then
   puts $wd
 end
 
+def statify(emitee)
+  stateA = ['a','@']
+  if stateA.include? emitee
+    return :a
+  else
+    return emitee.to_sym
+  end
+end
+def emitify(value)
+  emitNull = [:start,:end]
+  if emitNull.include? value
+    return ''
+  else
+    return value
+  end
+end
+
+pa = SmallAutomata.new()
+pa.start=:start
+total = 0
+
 fd = File.open("abtest.txt","w")
-$wd.each do |key, value|
-	fd.write "#{key}:\t{value}\n"
+$wd.each do |startState, transition|
+  total = transition.inject(0) {|result, (endState, value)| result+value}
+  transition.each do |endState, value|
+    pa.addEdge(statify(startState),statify(endState),emitify(endState),(1.0*value/total))
+	  fd.write "#{startState} to #{endState}:\t#{value}\n"
+  end
 end
 fd.close
+puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+puts pa.to_s
+puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
